61287506b9e50c5bac2ad62ba486df6228f83907
cyber-zeff <huzaifashamsi819@gmail.com>
2026-01-20 21:38:06 +0500

    add:first-program-in-go

---
+++ go/main.go
@@ -0,0 +1,12 @@
+// considered as new gens best lang
+// go lang can be used to build apps to build web apps - cloud
+// similarity with lots of lang like: c, java, pascal
+
+
+
+package main
+import ("fmt")
+
+func main() {
+	fmt.PrintIn("Hello World")
+}
